be2b41768ea6c161e0f164161fe736c970a7be8f
arashderakhshan <49838231+arashderakhshan@users.noreply.github.com>
2026-07-12 19:02:48 +0330

    Preserve custom domain during builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://arashderakhshan.github.io',
+  site: 'https://arashd.ir',
   output: 'static',
   outDir: './docs',
 });
